4014db21f8d99340a34b7ca6cfe0aa7cb44fea2d
Seth R Johnson <johnsonsr@ornl.gov>
2024-05-08 08:13:51 -0400

    Add version argument to CLI

--- celerpy/cli.py
+++ celerpy/cli.py
@@ -1,15 +1,29 @@
+from typing import Optional
+
 import typer
+from typing_extensions import Annotated
 
 from .settings import settings
 
 app = typer.Typer()
 
 
-@app.command(help=f"Display the current installed version of {settings.project_name}.")
-def version():
-    from . import _version
+def print_version(value: bool):
+    if value:
+        from . import _version
 
-    typer.echo(f"{settings.project_name} - {_version.version}")
+        typer.echo(f"{settings.project_name} {_version.version}")
+        raise typer.Exit()
+
+
+@app.command()
+def main(
+    version: Annotated[
+        Optional[bool],
+        typer.Option("--version", callback=print_version, is_eager=True),
+    ] = None,
+):
+    raise NotImplementedError("to do!")
 
 
 if __name__ == "__main__":
