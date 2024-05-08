7aefe556f41cd008af982b7bd0e95e75b644c48b
Seth R Johnson <johnsonsr@ornl.gov>
2024-05-08 16:33:16 -0400

    Add settings

--- celerpy/cli.py
+++ celerpy/cli.py
@@ -1,18 +1,31 @@
+# Copyright 2024 UT-Battelle, LLC, and other Celeritas developers.
+# See the top-level LICENSE file for details.
+# SPDX-License-Identifier: Apache-2.0
 from typing import Optional
 
 import typer
+from rich import print as rprint
 from typing_extensions import Annotated
-
-from .settings import settings
+from pydantic import ValidationError
 
 app = typer.Typer()
+
+
+def load_settings():
+    try:
+        from .settings import settings
+    except ValidationError as e:
+        rprint("[bold red]Failed[/bold red]", e)
+        raise typer.Exit()
+
+    return settings
 
 
 def print_version(value: bool):
     if value:
         from . import _version
 
-        typer.echo(f"{settings.project_name} {_version.version}")
+        typer.echo(_version.version)
         raise typer.Exit()
 
 
@@ -23,7 +36,7 @@ def main(
         typer.Option("--version", callback=print_version, is_eager=True),
     ] = None,
 ):
-    raise NotImplementedError("to do!")
+    rprint(load_settings().model_dump())
 
 
 if __name__ == "__main__":
--- celerpy/conf/settings.py
+++ celerpy/conf/settings.py
@@ -1,6 +1,21 @@
-from pydantic_settings import BaseSettings
+# Copyright 2024 UT-Battelle, LLC, and other Celeritas developers.
+# See the top-level LICENSE file for details.
+# SPDX-License-Identifier: Apache-2.0
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import DirectoryPath, Field
+from typing import Optional
 
 
 class Settings(BaseSettings):
-    project_name: str = "celerpy"
+    """Global settings for Celeritas front end.
+
+    Settings can be changed by setting environment variables such as
+    ``CELER_PREFIX_PATH`` (case insensitive).
+    """
+
+    model_config = SettingsConfigDict(env_prefix="celer_")
+
     debug: bool = False
+    prefix_path: Optional[DirectoryPath] = Field(
+        None, description="Path to the Celeritas build/install directory"
+    )
