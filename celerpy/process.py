aec664597795b0c793a74d1d92cbf5a4de18db10
Seth R Johnson <johnsonsr@ornl.gov>
2024-05-11 07:08:26 -0400

    Improve process and testing

--- celerpy/process.py
+++ celerpy/process.py
@@ -3,65 +3,74 @@
 # SPDX-License-Identifier: Apache-2.0
 """Manage a Celeritas process asynchronously with communication."""
 
-import json
 import os
 import subprocess
+from typing import Optional, TypeVar
+from pydantic import BaseModel
 
 from .settings import settings
 
 # TODO: from signal import SIGINT, SIGTERM
 
 
-def dump_json_line(obj, file):
-    """Write a single line of JSON, a newline, and flush."""
-    file.write(json.dumps(obj))
-    file.write("\n")
-    file.flush()
-
-
-def communicate(obj, process):
-    """Write a line of JSON and read a line of response.
+def communicate(process, line: str) -> Optional[str]:
+    """Write a line and read a line of response.
 
     For this to work, the child application *must* write a single line of
-    JSON formatted text, with nothing else, ending in a newline, and flush
-    immediately afterward.
+    text, ending in a newline, with nothing else, and flush immediately
+    afterward.
 
     If the file has already closed, this will return None.
 
     .. warning::
-       This method is susceptible to deadlock... so be careful
+       This method is susceptible to deadlock... so be careful!
     """
+    assert not line.endswith("\n")
+    stdin = process.stdin
+    stdout = process.stdout
     try:
-        dump_json_line(obj, process.stdin)
-        line = process.stdout.readline()
+        stdin.write(line)
+        stdin.write("\n")
+        stdin.flush()
+        return stdout.readline()
     except BrokenPipeError:
         return None
-    return json.loads(line) if line else None
 
 
-class Process(subprocess.Popen):
-    """Launch and manage a Celeritas process."""
+M = TypeVar("M", bound=BaseModel)
 
-    def __init__(self, executable, *, env=None, **kwargs):
-        # Set up environment variables
-        if env is None:
-            env = os.environ.copy()
-        for attr in ["color"]:
-            key = "CELER_" + attr.upper()
-            value = getattr(settings, attr)
-            if isinstance(value, bool):
-                # Set to 1 or blank
-                value = "1" if value else ""
-            env[key] = value
 
-        # Create child process, which implicitly keeps a copy of the file
-        # descriptors
-        super().__init__(
-            [settings.prefix_path / "bin" / executable, "-"],
-            stdin=subprocess.PIPE,
-            stdout=subprocess.PIPE,
-            bufsize=1,  # buffer by line
-            text=True,
-            env=env,
-            **kwargs,
-        )
+def communicate_model(process, model: BaseModel, expected_cls: type[M]) -> M:
+    """Communicate model to/from a process using JSON lines."""
+    result = communicate(process, model.model_dump_json())
+    if not result:
+        raise RuntimeError("Missing output from process")
+    return expected_cls.model_validate_json(result)
+
+
+def launch(executable: str, *, env=None, **kwargs):
+    """Set up and launch a Celeritas process."""
+    # Set up environment variables
+    if env is None:
+        env = os.environ.copy()
+    for attr in ["color"]:
+        key = "CELER_" + attr.upper()
+        value = getattr(settings, attr)
+        if isinstance(value, bool):
+            # Set to 1 or blank
+            value = "1" if value else ""
+        env[key] = value
+
+    # Create child process, which implicitly keeps a copy of the file
+    # descriptors
+    if settings.prefix_path is None:
+        raise RuntimeError("Celeritas prefix path is not set")
+    return subprocess.Popen(
+        [settings.prefix_path / "bin" / executable, "-"],
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        bufsize=1,  # buffer by line
+        text=True,
+        env=env,
+        **kwargs,
+    )
--- test/test_process.py
+++ test/test_process.py
@@ -3,47 +3,62 @@
 # SPDX-License-Identifier: Apache-2.0
 
 from pathlib import Path
+import json
 
 from celerpy.settings import settings
-from celerpy.process import Process, communicate
+from celerpy.process import launch, communicate
 
 settings.prefix_path = Path(__file__).parent / "mock-prefix"
 
 
+def communicate_json(process, inp):
+    out = communicate(process, json.dumps(inp))
+    if out:
+        return json.loads(out)
+    return None
+
+
 def test_manual():
-    p = Process("mock-process")
-    result = communicate("hello", p)
+    p = launch("mock-process")
+    result = communicate_json(p, "hello")
     assert result == "success"
-    result = communicate(["foo", "bar"], p)
+    result = communicate_json(p, ["foo", "bar"])
     assert result == ["success", ["foo", "bar"]]
-    result = communicate(None, p)
+    result = communicate_json(p, None)
     assert result == "closing"
     p.wait(timeout=0.5)
+    assert p.returncode == 0
+    p.stdout.close()
+    p.stdin.close()
+    del p
 
 
 def test_context():
-    with Process("mock-process") as p:
-        result = communicate("hello", p)
+    with launch("mock-process") as p:
+        result = communicate_json(p, "hello")
         assert result == "success"
-        result = communicate(["foo", "bar"], p)
+        result = communicate_json(p, ["foo", "bar"])
         assert result == ["success", ["foo", "bar"]]
+        print("closing process")
         # NOTE: we don't explicitly tell the process to exit cleanly, so
         # when `input()` gets an EOFError due to the stdin pipe closing, the
         # process tries to write "closing" to `stdout`, but that pipe has
-        # already been closed
+        # already been closed. I guess that also causes a BrokenPipeError in
+        # the attached pipe in this process.
+    print("closed process")
 
 
 def test_abort():
-    with Process("mock-process") as p:
-        result = communicate("hello", p)
+    with launch("mock-process") as p:
+        result = communicate_json(p, "hello")
         assert result == "success"
-        result = communicate("abort", p)
+        result = communicate_json(p, "abort")
         assert result is None
 
 
 def test_abort2():
-    with Process("mock-process") as p:
-        result = communicate("hello", p)
+    with launch("mock-process") as p:
+        result = communicate_json(p, "hello")
         assert result == "success"
         p.terminate()
-        result = communicate("terminating", p)
+        result = communicate_json(p, "terminating")
