336ddd7da36b9c0adba8931dad49498678ac8e5d
Seth R Johnson <johnsonsr@ornl.gov>
2026-05-07 10:53:38 -0400

    Update version and superficialities

--- celerpy/__init__.py
+++ celerpy/__init__.py
@@ -6,7 +6,7 @@ from importlib.metadata import PackageNotFoundError, version
 try:
     __version__ = version("celerpy")
 except PackageNotFoundError:
-    __version__ = "0.0.0-dev"
+    __version__ = "0.1.0-dev"
 
 # Expose __version__ for type checkers
 __all__ = ["__version__"]
